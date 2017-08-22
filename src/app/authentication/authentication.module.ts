3c79bae1ad00a456bfc62237a0ce3be58266c23a
ililcheva <ivailcheva@mail.bg>
2017-08-22 23:18:38 +0300

    Fixed some bugs in modules' routing

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'app/shared/shared.module';
+import { IsSignedInGuard } from 'app/authentication/guards/is-signed-in.guard';
+import { LoginComponent } from 'app/authentication/components/login/login.component';
+import { RegisterComponent } from 'app/authentication/components/register/register.component';
+import { authRouting } from 'app/authentication/authentication.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    authRouting
   ],
-  declarations: []
+  declarations: [LoginComponent, RegisterComponent],
+  providers: [IsSignedInGuard]
 })
 export class AuthenticationModule { }
