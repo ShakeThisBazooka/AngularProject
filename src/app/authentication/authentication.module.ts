0f2af5d66dace6617de2fef10642d0e3b585fc82
ililcheva <ivailcheva@mail.bg>
2017-08-23 00:00:04 +0300

    Merge conflict issues fully resolved

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { authRouting } from 'app/authentication/authentication.routes';
-=======
->>>>>>> f251c76a47cabde87aaa50e0028bf3c60e88f7f3
 import { SharedModule } from 'app/shared/shared.module';
 import { IsSignedInGuard } from 'app/authentication/guards/is-signed-in.guard';
 import { LoginComponent } from 'app/authentication/components/login/login.component';
 import { RegisterComponent } from 'app/authentication/components/register/register.component';
-<<<<<<< HEAD
-=======
-import { authRouting } from 'app/authentication/authentication.routes';
->>>>>>> f251c76a47cabde87aaa50e0028bf3c60e88f7f3
 
 @NgModule({
   imports: [
     CommonModule,
-<<<<<<< HEAD
     authRouting,
     SharedModule
-=======
-    SharedModule,
-    authRouting
->>>>>>> f251c76a47cabde87aaa50e0028bf3c60e88f7f3
   ],
   declarations: [LoginComponent, RegisterComponent],
   providers: [IsSignedInGuard]
