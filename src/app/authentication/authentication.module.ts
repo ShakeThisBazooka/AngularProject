f02f7d5554a720086ae34805e6e4ffb9df0a2480
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-08-24 11:09:44 +0300

    Initialized register-step-1.component and register-step-2.component.

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { authRouting } from 'app/authentication/authentication.routes';
 import { SharedModule } from 'app/shared/shared.module';
 import { IsSignedInGuard } from 'app/authentication/guards/is-signed-in.guard';
 import { LoginComponent } from 'app/authentication/components/login/login.component';
 import { RegisterComponent } from 'app/authentication/components/register/register.component';
-import { FormsModule } from '@angular/forms';
-import { MdCardModule, MdIconModule, MdButtonModule, MdTooltipModule, MdSidenavModule, MdInputModule } from '@angular/material';
+import {MdCardModule, MdInputModule, MdIconModule, MdButtonModule, MdTooltipModule, MdSidenavModule} from '@angular/material';
 import { RegisterStep1Component } from "./components/register/register-step-1/register-step-1.component";
 import { RegisterStep2Component } from "./components/register/register-step-2/register-step-2.component";
 
