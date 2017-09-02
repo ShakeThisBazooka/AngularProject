7b57166c5892b0056a227dcaeba3fb855825f214
VVoev <vladovoev@yahoo.co.uk>
2017-09-02 12:40:48 +0300

    fixed home

--- src/app/shared/services/home.service.ts
+++ src/app/shared/services/home.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable'
+import "rxjs/add/operator/map";
+
 
 @Injectable()
 export class HomeService {
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   getHomeInfo(): Observable<Response> {
-    return this.http.get('api/home');
+    return this.http.get('api/home').map(res => {
+      console.log(res.json())
+      return res.json()
+    })
   }
 }
