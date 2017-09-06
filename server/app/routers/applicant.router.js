731b2f26e5aec0319b0ebaefd3c55d96cefc4ba1
VVoev <vladovoev@yahoo.co.uk>
2017-09-06 19:21:11 +0300

    problem with JTW solved

--- server/app/routers/applicant.router.js
+++ server/app/routers/applicant.router.js
@@ -1,19 +1,18 @@
 const passport = require('passport');
 
+//passport.authenticate('jwt')
+
 
 const attachTo = (app, {applicantController}) => {
     app.get('/api/applicant/:id', ensureToken, applicantController.getById);
-    app.post('/api/applicant', passport.authenticate('jwt'), applicantController.createApplicant);
+    app.post('/api/applicant',ensureToken, applicantController.createApplicant);
     app.put('/api/applicant/:id', applicantController.updateApplicant);
 
   };
 
 function ensureToken(req,res,next) {
-  const bearerHeader = req.headers["token"];
-  console.log(bearerHeader);
-  const bearer = bearerHeader.split(' ');
-  const bearerToken = bearer[1];
-  req.token = bearerToken;
+  const token = req.headers["token"];
+  req.token = token;
   next();
 }
 
