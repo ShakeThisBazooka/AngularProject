4ffb43d4ca44955caddc3094bd51be70c4be7c21
ililcheva <ivailcheva@mail.bg>
2017-09-08 17:32:26 +0300

    Changed port

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-const port = 3000;
+const port = process.env.PORT || 3000;
 const connectionString = 'mongodb://ililcheva:123456qQ@ds129374.mlab.com:29374/heroku_4hqds5km';
 
 module.exports = { port, connectionString };
