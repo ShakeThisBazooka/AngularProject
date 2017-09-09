3baa8ac003d2805a3d2039ba892eaea73dfe0c4e
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-09-09 19:28:01 +0300

    Added applyForJob method in job details component.

--- src/app/front/components/job-detail/job-detail.component.ts
+++ src/app/front/components/job-detail/job-detail.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { ApplicantService } from '../../../shared/services/applicant.service';
+import { Job } from './../../../shared/models/job';
+import { JobService } from './../../../shared/services/job.service';
 import { Location } from '@angular/common';
+import { Response } from '@angular/http';
+import { UserService } from './../../../shared/services/user.service';
 
 @Component({
   selector: 'app-job-detail',
@@ -8,9 +15,47 @@ import { Location } from '@angular/common';
 })
 export class JobDetailComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  private job;
+  public value: string;
+  public isCompanyJob: boolean;
+
+  constructor(private location: Location, private applicantService: ApplicantService,
+  private userService: UserService, private route: ActivatedRoute,
+  private jobService: JobService) { }
+
+  getJob(id: string) {
+    this.jobService.getOne(id)
+          .subscribe((job: Job) => {
+              this.job = job;
+        });
+  }
 
   ngOnInit() {
+    const userInfo = this.userService.getUserInfo();
+    const userRole = userInfo.role;
+
+    if (userRole === 'applicant') {
+      this.value = 'Apply';
+    } else {
+      const jobId = this.route.params['_value'].id;
+      this.getJob(jobId);
+
+      if (this.job.companyId === userInfo.userId) {
+        this.isCompanyJob = true;
+      } else {
+        this.isCompanyJob = false;
+      }
+      this.value = 'Edit';
+    }
+  }
+  applyJobDetail() {
+    const userInfo = this.userService.getUserInfo();
+    const userId = userInfo.userId;
+    const jobId = this.route.params['_value'].id;
+
+    if (userId !== undefined) {
+      return this.applicantService.apply(userId, jobId);
+    }
   }
 
 }
