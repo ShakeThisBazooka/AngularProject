57299c74e971a556e99672d4af22f30221ca27ab
ililcheva <ivailcheva@mail.bg>
2017-09-09 13:42:47 +0300

    Changed jobs.router endpoint

--- server/app/routers/jobs.router.js
+++ server/app/routers/jobs.router.js
@@ -1,6 +1,6 @@
 const attachTo = (app, {jobsController}) => {
-  app.get('/jobs', jobsController.getJobs);
-  app.get('/jobs/:id', jobsController.getJob);
+  app.get('/api/jobs', jobsController.getJobs);
+  app.get('/api/jobs/:id', jobsController.getJob);
 };
 
 module.exports = {attachTo};
