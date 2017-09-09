4eba1f51abf04ff8584de5a14ed2c241ae02ec3b
ililcheva <ivailcheva@mail.bg>
2017-09-10 02:47:14 +0300

    Added ensureToken to all private routes

--- server/app/routers/applicant.router.js
+++ server/app/routers/applicant.router.js
@@ -6,10 +6,10 @@ const passport = require('passport');
 const attachTo = (app, {applicantController}) => {
     app.get('/api/applicant/:id', ensureToken, applicantController.getById);
     app.get('/api/applicant/:id/jobs', ensureToken, applicantController.getJobs);
-    app.post('/api/applicant', applicantController.createApplicant);
-    app.post('/api/applicant/:aid/apply/:jid', applicantController.addJobToApplicant);
-    app.put('/api/applicant/:id', applicantController.updateApplicant);
-    app.delete('/api/applicant/:id', applicantController.deleteApplicant);
+    app.post('/api/applicant', ensureToken, applicantController.createApplicant);
+    app.post('/api/applicant/:aid/apply/:jid', ensureToken, applicantController.addJobToApplicant);
+    app.put('/api/applicant/:id', ensureToken, applicantController.updateApplicant);
+    app.delete('/api/applicant/:id', ensureToken, applicantController.deleteApplicant);
   };
 
 function ensureToken(req,res,next) {
--- server/app/routers/company.router.js
+++ server/app/routers/company.router.js
@@ -10,7 +10,7 @@ const attachTo = (app, {companyController}) => {
     app.put('/api/company/:id', ensureToken, companyController.updateCompany);
     app.put('/api/company/:cid/jobs/:jid', ensureToken, companyController.updateJob);
     app.delete('/api/company/:id', ensureToken, companyController.deleteCompany);
-    app.delete('/api/company/:cid/jobs/:jid', companyController.deleteJob);
+    app.delete('/api/company/:cid/jobs/:jid', ensureToken, companyController.deleteJob);
   };
 
 function ensureToken(req,res,next) {
